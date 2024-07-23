0e086f6176d0e02ec6352660bd70dce3d0226ad5
Andoni1Barba <Andoni1barba@gmail.com>
2024-07-23 10:45:09 -0400

    commit show

---
+++ public/assets/js/scriptpage.js
@@ -0,0 +1,9 @@
+let btnMenu = document.getElementById('btn-menu');
+let menu = document.getElementById('menu');
+let navbar = document.querySelector('.navbar');
+
+btnMenu.addEventListener('click', function() {
+  menu.classList.toggle('active');
+  navbar.classList.toggle('active');
+  btnMenu.classList.toggle('active');
+});
